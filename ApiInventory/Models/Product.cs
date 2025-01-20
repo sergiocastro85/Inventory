fe39f75ceef03c0548c81b8b96f39a5e05d8714d
sergiocastro85 <sergiocastro86@gmail.com>
2025-01-19 20:53:53 -0500

    First commit

---
+++ ApiInventory/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiInventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
